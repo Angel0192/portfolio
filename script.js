7eb69cb84b8c5ea7cd4c5e89922a1796619cd926
Angel0192 <angellope904@icloud.com>
2024-11-14 18:13:24 -0600

    massive overhall

--- script.js
+++
@@ -1,7 +0,0 @@
-const currentUrl = window.location.pathname;
-  const navLinks = document.querySelectorAll('.nav-links');
-  navLinks.forEach(link => {
-    if (link.getAttribute('href') === currentUrl) {
-      link.classList.add('active');
-    }
-  });
